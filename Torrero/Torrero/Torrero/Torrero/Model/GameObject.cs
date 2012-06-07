25df6c0783f578159084b06148d27cf1b7f51cf9
Dominik Schinner <5451934+doschi@users.noreply.github.com>
2012-06-07 22:44:33 +0200

    renamed Title

---
+++ Torrero/Torrero/Torrero/Torrero/Model/GameObject.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Torrero.Model
+{
+    public abstract class GameObject
+    {
+        // All getters, setters.
+        #region Properties
+
+        /// <summary>
+        /// Position of the object.
+        /// </summary>
+        public Vector2 BottomLeftPosition
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        /// <summary>
+        /// Texture of the object.
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        /// <summary>
+        /// Size of the object.
+        /// </summary>
+        public Vector2 Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        /// <summary>
+        /// Center of the Object.
+        /// </summary>
+        public Vector2 CenterPosition
+        {
+            get { return (position + size / 2); }
+            set { position = value - size / 2; }
+        }
+
+        /// <summary>
+        /// Top coordinate of the object.
+        /// </summary>
+        public float Top
+        {
+            get { return position.Y + size.Y; }
+            set { position.X = value - size.Y; }
+        }
+
+        /// <summary>
+        /// Left coordinate of the object.
+        /// </summary>
+        public float Left
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+
+        /// <summary>
+        /// Right coordinate of the object.
+        /// </summary>
+        public float Right
+        {
+            get { return position.X + size.X; }
+            set { position.X = value - size.X; }
+        }
+
+        /// <summary>
+        /// Rotation in radians.
+        /// </summary>
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        #endregion
+
+        // All members. They should always be private and provide a getter/setter if necessary.
+        #region Members
+
+        private Vector2 position;
+
+        private Texture2D texture;
+
+        private Vector2 size;
+
+        /// <summary>
+        /// Rotation in radians.
+        /// </summary>
+        private float rotation;
+
+        #endregion
+
+        // Object constructors.
+        #region Initialization
+
+        public GameObject(Vector2 position, Vector2 size, Texture2D texture)
+        {
+            this.position = position;
+            this.size = size;
+            this.texture = texture;
+        }
+
+        #endregion
+    }
+}
---
+++ Volley-BestehendesProjekt/Volley2/Volley2/Menu/ScoreIndicator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Volley2.Menu
+{
+    public class ScoreIndicator
+    {
+        
+        // Variables
+
+        private Texture2D graphics;
+        public Texture2D Graphics
+        {
+            get { return graphics; }
+            set { graphics = value; }
+        }
+
+        private Vector2 size;
+        public Vector2 Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        private SpriteFont font;
+        public SpriteFont Font
+        {
+            get { return font; }
+            set { font = value; }
+        }
+
+        private Vector4 score;
+        public Vector4 Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        // Constructors
+
+        public ScoreIndicator()
+        {
+            size = new Vector2(200, 100);
+        }
+
+
+        // Methods
+
+        public void SetScore(Vector2 newScore)
+        {
+            int temp1 = (int)score.X;
+            int temp2 = (int)newScore.X;
+            if (temp2 > 9)
+            {
+                temp1 = temp2 / 10;
+                temp2 = temp2 % 10;
+            }
+
+            int temp3 = (int)score.Z;
+            int temp4 = (int)newScore.Y;
+            if (temp4 > 9)
+            {
+                temp3 = temp4 / 10;
+                temp4 = temp4 % 10;
+            }
+
+            score = new Vector4(temp1, temp2, temp3, temp4);
+        }
+
+
+    }
+}
