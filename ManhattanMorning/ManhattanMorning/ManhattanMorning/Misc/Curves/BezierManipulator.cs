b2d6c5d348d9413784dade74a4616026becb0e89
derjuli <derjuli@localhost>
2012-06-08 17:09:58 +0200

    - improved vulcano - shortened fading into levels

---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/Curves/BezierManipulator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ManhattanMorning.Misc.Curves
+{
+
+    class BezierManipulator : Manipulator
+    {
+        
+        BezierBatch bezierBatch;
+        private int selectedNode = -1;
+        private bool isActive = false;
+
+
+        /// <summary>
+        /// To see if the manipulator is active at the moment
+        /// </summary>
+        public override bool IsActive
+        {
+            get { return this.isActive; }
+        }
+
+        public BezierManipulator(BezierBatch bezierBatch)
+        {
+            this.bezierBatch = bezierBatch;
+            
+        }
+
+
+        /// <summary>
+        /// Handles the mouse input and moves points when necessary
+        /// </summary>
+        /// <param name="mouseState">the current updated mouse state</param>
+        override public void handleInteraction(MouseState mouseState)
+        {
+            
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                
+                foreach (Vector2 v in this.bezierBatch.getPoints())
+                {
+                    if (selectedNode == -1)
+                    {
+                        if (Math.Abs(v.X*100f - mouseState.X) <= 20 && Math.Abs(v.Y*100f - mouseState.Y) <= 20)
+                        {
+                            this.isActive = true;
+                            selectedNode = bezierBatch.getPoints().IndexOf(v);
+                        }
+                    }
+
+                }
+            }
+            else
+            {
+                if (selectedNode != -1)
+                {
+                    Console.WriteLine("ID: " + selectedNode + "   New Position: X:" + mouseState.X*0.01f + "  Y:" + mouseState.Y*0.01f);
+                }
+                selectedNode = -1;
+                this.isActive = false;
+            }
+            if (selectedNode >= 0)
+            {
+                mouseState = Mouse.GetState();
+                Vector2  tmp = this.bezierBatch.getPoints().ElementAt(selectedNode);
+                tmp.X = mouseState.X*0.01f;
+                tmp.Y = mouseState.Y*0.01f;
+                this.bezierBatch.getPoints()[selectedNode] = tmp;
+                this.bezierBatch.update();
+
+            }
+        }
+
+
+        public bool pointSelected()
+        {
+            if (selectedNode == -1) return false;
+            else return true;
+        }
+
+    }
+}
---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Model/GameObject/PowerUpObjects/Bomb.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using ManhattanMorning.Misc;
+
+namespace ManhattanMorning.Model.GameObject
+{
+    public class Bomb : PowerUpObject
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// References to body of every object in farseer engine
+        /// </summary>
+        public bool Exploded { get { return exploded; } set { exploded = value; } }
+
+        /// <summary>
+        /// Is SuperBomb or not.
+        /// </summary>
+        public bool IsSuperBomb { get { return isSuperBomb; } set { isSuperBomb = value; } }
+
+        #endregion
+
+        #region Members
+
+        private bool exploded;
+
+        private bool isSuperBomb;
+
+        #endregion
+
+
+        #region Initialization
+
+        /// <summary>
+        /// Initializes a Bomb object with an animation.
+        /// </summary>
+        /// <param name="texture">The texture. if this object has no texture, set to null</param>
+        /// <param name="animation">animation, if this object has no animation, set to null</param>
+        /// <param name="Size">The size. depending on the measurementUnit either in Meters or in Pixel</param>
+        /// <param name="Position">The upper left position. depending on the measurementUnit either in Meters or in Pixel </param>
+        /// <param name="layer">In which layer should the object be drawn?</param>
+        /// <param name="measurementUnit">The measurement unit. </param>
+        /// <param name="ballIndicator">Reference to its ball indicator</param>
+        public Bomb(Texture2D texture, Texture2D shadowTexture, SpriteAnimation animation, Vector2 size, Vector2 position, int layer, MeasurementUnit measurementUnit, bool superBomb)
+            : base(texture, shadowTexture, animation, size, position, layer, measurementUnit)
+        {
+            exploded = false;
+            isSuperBomb = superBomb;
+
+            if (superBomb)
+            {
+                Name = "Bomb";
+            }
+
+        }
+
+        #endregion
+
+
+    }
+}
