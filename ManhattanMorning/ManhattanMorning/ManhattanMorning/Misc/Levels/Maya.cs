db592f3df7a0f75336542adcb45c71644b4ac5f5
JojoSchmidt <JojoSchmidt@localhost>
2012-06-08 17:29:36 +0200

    Deactivated backbutton on gamepads

---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/Levels/Maya.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using ManhattanMorning.Controller;
+using ManhattanMorning.Misc.Curves;
+using ManhattanMorning.Model.GameObject;
+using ManhattanMorning.View;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace ManhattanMorning.Misc.Levels
+{
+    class Maya : Level
+    {
+
+        private Vector2 levelSize;
+
+        /// <summary>
+        /// Initializes the level
+        /// </summary>
+        public Maya()
+            : base()
+        {
+
+            #region Static Levelproperties
+
+            // Define name of the level
+            levelProperties.Add("LevelName", "Maya");
+
+            // Define the size of the level (in meters, has to have 16:9 ratio!)
+            levelSize = new Vector2(12.8f, 7.2f);
+            levelProperties.Add("LevelSize", levelSize);
+
+            // Define the reset positions for ball, left team, right team
+            // (in meters, origin is the upper left corner)
+            levelProperties.Add("BallResetPosition", new Vector2(levelSize.X / 2 - ((Vector2)SettingsManager.Instance.get("ballSize")).X * 0.5f, 1.0f));
+            levelProperties.Add("LeftTeamResetPosition", new Vector2((levelSize.X / 4f - ((float)SettingsManager.Instance.get("playerSize")) * 0.5f), 5.0f));
+            levelProperties.Add("RightTeamResetPosition", new Vector2((levelSize.X / 4f * 3 - ((float)SettingsManager.Instance.get("playerSize")) * 0.5f), 5.0f));
+
+            #endregion
+        }
+
+        override public void load()
+        {
+
+            #region Define net
+
+            // Define net size and texture
+            // it's centered automatically
+
+            // Size of net (pixel) is always 28x280 px
+            Vector2 screenResolution = (Vector2)SettingsManager.Instance.get("screenResolution");
+            levelObjectsList.Add(new Net("net", new Vector2(((28 / screenResolution.X) * levelSize.X), ((280 / screenResolution.Y) * levelSize.Y))));
+
+            #endregion
+
+
+            #region Define ball
+
+            // Define ball texture
+            // This definition is used for all balls
+
+            levelObjectsList.Add(new Ball("Ball"));
+
+            #endregion
+
+
+            #region Define powerups
+
+            // Set allowed powerups
+
+            //AllowedPowerUps.Add(PowerUpType.BallRain);
+            //AllowedPowerUps.Add(PowerUpType.SuperBomb);
+            //AllowedPowerUps.Add(PowerUpType.DoubleBall);
+            AllowedPowerUps.Add(PowerUpType.InvertedControl);
+            //AllowedPowerUps.Add(PowerUpType.Jumpheight);
+            //AllowedPowerUps.Add(PowerUpType.Wind);
+
+            //AllowedPowerUps.Add(PowerUpType.Volcano);
+            //AllowedPowerUps.Add(PowerUpType.SunsetSunrise);
+            AllowedPowerUps.Add(PowerUpType.SwitchStones);   
+
+            #endregion
+
+
+            #region Define PassivObjects
+
+            // Define all passive objects
+            // Passive objects are all objects which have no influence in the actual game
+            // -> just visuals
+            // you can also add an animation and/or an animation path to these objects
+
+            levelObjectsList.Add(new PassiveObject("Background", "background", levelSize, Vector2.Zero, 0));
+
+            levelObjectsList.Add(new PassiveObject("Front", "front", levelSize, Vector2.Zero, 60));
+
+            // add stone objects. they don't have any collision, this functionality is added by active stoneBlockers
+            PassiveObject stone0 = new PassiveObject("stone0", "stone0", Graphics.Instance.convertUnits(new Vector2(96, 87), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), Graphics.Instance.convertUnits(new Vector2(120, 148), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), 25);
+
+            PassiveObject stone1 = new PassiveObject("stone1", "stone1", Graphics.Instance.convertUnits(new Vector2(88, 81), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), Graphics.Instance.convertUnits(new Vector2(357, 138), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), 25);
+
+            PassiveObject stone2 = new PassiveObject("stone2", "stone2", Graphics.Instance.convertUnits(new Vector2(69, 72), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), Graphics.Instance.convertUnits(new Vector2(685, 146), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), 25);
+
+            PassiveObject stone3 = new PassiveObject("stone3", "stone3", Graphics.Instance.convertUnits(new Vector2(98, 84), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), Graphics.Instance.convertUnits(new Vector2(852, 151), Model.MeasurementUnit.Pixel, Model.MeasurementUnit.Meter), 25);
+
+
+            levelObjectsList.Add(stone0);
+            levelObjectsList.Add(stone1);
+            levelObjectsList.Add(stone2);
+            levelObjectsList.Add(stone3);
+             
+
+            #endregion
+
+        }
+
+            
+
+    }
+}
