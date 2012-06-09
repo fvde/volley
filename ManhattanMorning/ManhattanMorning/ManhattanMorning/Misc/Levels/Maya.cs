e4c3ecb7f9c72a957fd9569ce2faa4169894f368
JojoSchmidt <JojoSchmidt@localhost>
2012-06-09 17:46:57 +0200

    - Replaced videos - realistic settings

--- ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/Levels/Maya.cs
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/Levels/Maya.cs
@@ -74,12 +74,12 @@ namespace ManhattanMorning.Misc.Levels
 
             // Set allowed powerups
 
-            //AllowedPowerUps.Add(PowerUpType.BallRain);
-            //AllowedPowerUps.Add(PowerUpType.SuperBomb);
-            //AllowedPowerUps.Add(PowerUpType.DoubleBall);
-            //AllowedPowerUps.Add(PowerUpType.InvertedControl);
+            AllowedPowerUps.Add(PowerUpType.BallRain);
+            AllowedPowerUps.Add(PowerUpType.SuperBomb);
+            AllowedPowerUps.Add(PowerUpType.DoubleBall);
+            AllowedPowerUps.Add(PowerUpType.InvertedControl);
             AllowedPowerUps.Add(PowerUpType.Jumpheight);
-            //AllowedPowerUps.Add(PowerUpType.Wind);
+            AllowedPowerUps.Add(PowerUpType.Wind);
 
             //AllowedPowerUps.Add(PowerUpType.Volcano);
             //AllowedPowerUps.Add(PowerUpType.SunsetSunrise);
