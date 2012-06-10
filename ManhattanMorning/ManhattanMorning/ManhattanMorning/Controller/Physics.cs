801b103b6ddf3faf4d71b7b6c5302845611bcabb
derjuli <derjuli@localhost>
2012-06-10 17:16:15 +0200

    bomb rotation

--- ManhattanMorning/ManhattanMorning/ManhattanMorning/Controller/Physics.cs
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Controller/Physics.cs
@@ -1255,6 +1255,7 @@ namespace ManhattanMorning.Controller
             // Create the red background
             PassiveObject bomb_red = new PassiveObject("Bomb_red", true, StorageManager.Instance.getTextureByName("PowerUp_Bomb_red"), null, null,
                 (Vector2)SettingsManager.Instance.get("superBombSize"), position, 57, MeasurementUnit.Meter);
+            bomb_red.RotateWithOffset = true;
 
             // Attach it to the bomb
             bomb.attachObject(bomb_red);
