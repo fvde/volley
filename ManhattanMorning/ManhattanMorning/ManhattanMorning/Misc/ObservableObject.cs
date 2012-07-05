d81623971daa1463781a999aacda9b8d02327d56
derjuli <derjuli@localhost>
2012-07-05 21:14:34 +0200

    Fixed Index Bug. 4 Sandfountains are now always created regardless of the player count

---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/ObservableObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManhattanMorning.Misc
+{
+    /// <summary>
+    /// Objects inheriting from this class can be observed and can notify their registered Observers.
+    /// </summary>
+    public class ObservableObject
+    {
+
+        #region Properties
+        //all Properties right here
+        #endregion
+
+        #region Members
+
+        //List of all the registered Observers
+        private List<IObserver> observerList = new List<IObserver>();
+
+        //The current Instance of the Logger
+        Logger logger = Logger.Instance;
+
+        #endregion
+
+        /// <summary>
+        /// Register a new Observer to the Observable Object
+        /// </summary>
+        /// <param name="observer">The Observer that want to listen</param>
+        public void registerObserver(IObserver observer)
+        {
+            observerList.Add(observer);
+            logger.log(Sender.ObservableObject, observer.ToString() + " added as Observer to "+this.ToString(), PriorityLevel.Priority_1);
+        }
+
+        /// <summary>
+        /// Remove an existing Observer from an Observable Object
+        /// </summary>
+        /// <param name="observer">The Observer to be removed</param>
+        public void removeObserver(IObserver observer)
+        {
+            observerList.Remove(observer);
+            logger.log(Sender.ObservableObject, observer.ToString() + " was removed as Observer from "+this.ToString(),PriorityLevel.Priority_1);
+        }
+
+        /// <summary>
+        /// Notify all registered Observers
+        /// </summary>
+        public void notifyObservers()
+        {
+            foreach (IObserver observer in observerList)
+            {
+                logger.log(Sender.ObservableObject, "Notifying " + observer.ToString(),PriorityLevel.Priority_2);
+                observer.notify(this);
+            }
+        }
+
+    }
+}
---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Model/GameObject/PowerUpObjects/PowerUpObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using ManhattanMorning.Misc;
+
+namespace ManhattanMorning.Model.GameObject
+{
+    public abstract class PowerUpObject : ActiveObject
+    {
+
+        #region Properties
+
+
+        #endregion
+
+        #region Members
+
+
+        #endregion
+
+
+        #region Initialization
+
+        /// <summary>
+        /// Initializes a PowerUpObject object with an animation.
+        /// </summary>
+        /// <param name="name">Name of the Object. use variable name, if not sure what to choose.</param>
+        /// <param name="visible">Shall the object be visible? only set visible if object  either contains a texture or a animation</param>
+        /// <param name="texture">The texture. if this object has no texture, set to null</param>
+        /// <param name="animation">animation, if this object has no animation, set to null</param>
+        /// <param name="Size">The size. depending on the measurementUnit either in Meters or in Pixel</param>
+        /// <param name="Position">The upper left position. depending on the measurementUnit either in Meters or in Pixel </param>
+        /// <param name="layer">In which layer should the object be drawn?</param>
+        /// <param name="measurementUnit">The measurement unit. </param>
+        /// <param name="ballIndicator">Reference to its ball indicator</param>
+        public PowerUpObject(Texture2D texture, Texture2D shadowTexture, SpriteAnimation animation, Vector2 size, Vector2 position, int layer, MeasurementUnit measurementUnit)
+            : base("PowerUpObject", true, texture, shadowTexture, animation, size, position, layer, measurementUnit)
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
