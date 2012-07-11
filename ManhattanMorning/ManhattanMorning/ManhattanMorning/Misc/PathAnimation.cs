7bfdd028526267f0588daa60fcfc22bab2d5aebb
Dominik Schinner <5451934+doschi@users.noreply.github.com>
2012-07-11 15:14:59 +0200

    turned simulateTrialMode off

---
+++ ManhattanMorning/ManhattanMorning/ManhattanMorning/Misc/PathAnimation.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using ManhattanMorning.Model;
+using ManhattanMorning.Model.GameObject;
+using ManhattanMorning.Controller;
+using ManhattanMorning.Misc.Curves;
+
+namespace ManhattanMorning.Misc
+{
+    public class PathAnimation
+    {
+
+        #region Members
+        private DrawableObject correspondingObject;
+        private Path path;
+        private float animationDuration = 1000f;
+        private bool active;
+        private float elapsedTime;
+        private bool activateRoation = false;
+        private bool playOnce = false;
+        
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Sets the Object that has the Path so that we dont have to pass the corresponding
+        /// Object all the time.
+        /// </summary>
+        public DrawableObject CorrespondingObject
+        {
+            get { return correspondingObject; }
+            set
+            {
+                correspondingObject = value;
+            }
+        }
+
+        public Path Path
+        {
+            get { return this.path; }
+            set { this.path = value; }
+        }
+
+        public bool ActivateRotation
+        {
+            get { return this.activateRoation; }
+            set { this.activateRoation = value; }
+        }
+
+        public float ElapsedTime
+        {
+            get { return elapsedTime; }
+            set { elapsedTime = value; }
+        }
+
+        public bool PlayOnce
+        {
+            get { return playOnce; }
+            set { playOnce = value; }
+        }
+
+        /// <summary>
+        /// The duration of the animation in milliseconds
+        /// </summary>
+        public float AnimationDuration { get { return this.animationDuration; } set { this.animationDuration = value; } }
+ 
+        /// <summary>
+        /// Used to set the waypoints active or inactive. In inactive state the object will be drawn but does not move.
+        /// </summary>
+        public bool Active { get { return active; } set { active = value; } }
+
+        #endregion
+
+        #region Init
+
+        public PathAnimation(Path path)
+        {
+            this.path = path;
+            this.active = true;
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Updates the position of the Object that is linked to the PathAnimation
+        /// </summary>
+        /// <param name="gameTime">the current game time</param>
+        public void updatePosition(GameTime gameTime)
+        {
+            if (elapsedTime > animationDuration) return;
+
+            elapsedTime += (float)(gameTime.ElapsedGameTime.TotalMilliseconds);
+            float t =  elapsedTime / animationDuration;
+            Vector2 newPosition = path.evaluate(t);
+            newPosition = newPosition - correspondingObject.Size * 0.5f;
+            correspondingObject.Position = newPosition;
+
+            if (activateRoation)
+            {
+                Vector2 dir = path.getDirectionAtValue(t);
+                Vector2 neutral = new Vector2(1, 0);
+                correspondingObject.Rotation = (dir.Y > neutral.Y ? 1f : -1f) * (float)Math.Acos(Vector2.Dot(dir, neutral));
+            }
+
+            if(t >= 1 && !playOnce) elapsedTime = 0;
+        }
+
+        /// <summary>
+        /// Resets the objects position to the starting point of its path.
+        /// </summary>
+        public void resetPosition(){
+            elapsedTime = 0f;
+        }
+
+        #endregion
+    }
+}
---
+++ Torrero/Torrero/Torrero/Torrero/TorreroConstants.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Torrero
+{
+    public class TorreroConstants
+    {
+
+        /// <summary>
+        /// The maximum distance between player and nextTile position. If the difference is smaller, the players position will snap to the tiles position.
+        /// The movingTreshhold is also used when checking if a GameObject is completely on a Tile.
+        /// </summary>
+        public const int movingTreshhold = 4;
+
+        /// <summary>
+        /// The Viewport heigth minus the current Tilesize. Used for drawing conversions.
+        /// </summary>
+        public const float ViewportHeightWithTile = 800 - TileSize;
+
+        /// <summary>
+        /// Size of all tiles ingame.
+        /// </summary>
+        public const int TileSize = 60;
+
+        /// <summary>
+        /// Half size of all tiles ingame.
+        /// </summary>
+        public const int TileSizeHalf = TileSize / 2;
+
+        /// <summary>
+        /// Number of rows that the grid must contain.
+        /// </summary>
+        public const int RowNumber = 16;
+
+        /// <summary>
+        /// Number of columns: ATTENTION the first and the last are just for visual effects and to give the player room to maneuver!
+        /// </summary>
+        public const int ColumnNumber = 9;
+
+        /// <summary>
+        /// Minimum of rows that are created when calculating a new block
+        /// </summary>
+        public const int MinimumBlockSize = 2;
+
+        /// <summary>
+        /// Maximum of rows that are created when calculating a new block
+        /// </summary>
+        public const int MaximumBlockSize = 4;
+
+        /// <summary>
+        /// Horizontal maximum speed of the player.
+        /// </summary>
+        public const float HorizontalMaximumSpeed = 250;
+
+        /// <summary>
+        /// Vertical maximum speed of the player.
+        /// </summary>
+        public const float VerticalMaximumSpeed = 250f;
+
+        /// <summary>
+        /// Horizontal maximum speed of the player.
+        /// </summary>
+        public const float HorizontalSpeed = 120.0f;
+
+        /// <summary>
+        /// Vertical speed of the player.
+        /// </summary>
+        public const float VerticalSpeed = 120.0f;
+
+        /// <summary>
+        /// When the touch location is closer then this value to the player, the player will move forward.
+        /// </summary>
+        public const int StraightMovementVariation = 10;
+
+        /// <summary>
+        /// Likelyhood of starting a new path.
+        /// </summary>
+        public static float CrossingProbability = 0.4f;
+
+        /// <summary>
+        /// Likelyhood of turning in pathes.
+        /// </summary>
+        public static float TurnProbability = 0.2f;
+
+        /// <summary>
+        /// Likelyhood of continuing a side path
+        /// </summary>
+        public static float ContinueSidePathProbability = 0.7f;
+
+        /// <summary>
+        /// Every interval the difficulty is increased by (1-Probability) * DifficultyGrowthFactor for all probabilities and directly for all speedsss.
+        /// </summary>
+        public const float DifficultyGrowthFactor = 0.05f;
+
+        /// <summary>
+        /// Determines how often the difficulty is increased (ms)
+        /// </summary>
+        public const int DifficultyInterval = 3000;
+    }
+}
